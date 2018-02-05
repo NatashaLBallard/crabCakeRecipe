65c93691654eebf56ac51c3810532fcf004059ae
natashalballard <nbitcareer@gmail.com>
2018-02-05 16:55:55 -0500

    Initial commit

---
+++ src/main/java/com/crabcakerecipe/demo/CrabCakeRecipeApplication.java
@@ -0,0 +1,12 @@
+package com.crabcakerecipe.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrabCakeRecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrabCakeRecipeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/crabcakerecipe/demo/CrabCakeRecipeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.crabcakerecipe.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CrabCakeRecipeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
