fe8d9afcfa67190f161ec21a62b110cd53bce3ff
natashalballard <nbitcareer@gmail.com>
2018-02-06 11:46:48 -0500

    added recipe pages

--- src/main/java/com/crabcakerecipe/demo/MainController.java
+++ src/main/java/com/crabcakerecipe/demo/MainController.java
@@ -7,9 +7,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MainController {
 
     @RequestMapping("/")
+    public String index (){
+        return "index";
+    }
+
+    @RequestMapping("/crabcake")
     public String crabpage (){
         return "crabCakeRecipe";
     }
 
+    @RequestMapping("/injera")
+    public String injerapage (){
+        return "injeraRecipe";
+    }
 
+    @RequestMapping("/golabjamun")
+    public String golabjamunpage (){
+        return "golabJamunRecipe";
+    }
+
+    @RequestMapping("/jerkchicken")
+    public String jerkchickenpage (){
+        return "jerkChickenRecipe";
+    }
 }
